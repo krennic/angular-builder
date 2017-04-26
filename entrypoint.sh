76dc238096cf148164362ce4f5be99c792d30676
Oihid Belounis <oihid-externe.belounis@edf.fr>
2017-04-26 13:48:22 +0200

    Npm login use env var token

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,9 @@ if [ "$ENABLE_FF" == "TRUE" ]; then
 	/etc/init.d/xvfb start && sleep 5
 fi
 
+#npm login
+if [ -z "$NPM_URL" && -z "$NPM_TOKEN" ]; then
+	echo -e "$NPM_URL/repository/npm-snapshots/:_authToken=$NPM_TOKEN\n$NPM_URL/repository/npm-releases/:_authToken=$NPM_TOKEN" > /root/.npmrc
+fi
+
 exec "$@"
